6033be7fce1169e1ec67e0e141f89da6339b3a82
Ярослав <myn4ck@gmail.com>
2025-12-14 15:17:47 +0500

    Update client_esp32.py

--- src/client_esp32.py
+++ src/client_esp32.py
@@ -2,16 +2,14 @@ import network
 import usocket as socket
 import time
 import ujson
-from machine import Pin, ADC
+from machine import Pin
 import dht
 
+#SSID = "Название сети"
+#PASS = "Пароль сети"
 
-# SSID = "%s"
-# PASS = "%s"
-
-# API_HOST = "%s"
-# API_PORT = %s
-
+#API_HOST = "ip хоста"
+#API_PORT = 'порт хоста !!! кавычки убрать'
 
 wifi = network.WLAN(network.STA_IF)
 wifi.active(True)
@@ -24,15 +22,8 @@ while not wifi.isconnected():
 print("Wi-Fi подключён:", wifi.ifconfig()[0])
 
 sensor = dht.DHT11(Pin(4))
-adc = ADC(Pin(36))
-adc.atten(ADC.ATTN_11DB)
 
-def read_internal_temp():
-    raw = adc.read()
-    voltage = raw / 4095 * 3.3
-    temp_c = 27 - (voltage - 0.706)/0.001721
-    return round(temp_c, 1)
-    
+
 def send_data(temp, humidity):
     try:
         addr = socket.getaddrinfo(API_HOST, API_PORT)[0][-1]
@@ -47,25 +38,23 @@ def send_data(temp, humidity):
 
         data = s.recv(1024)
         print("Ответ сервера:", data.decode())
+
     except Exception as e:
         print("Ошибка:", e)
     finally:
         s.close()
 
+
 while True:
     try:
-        try:
-            sensor.measure()
-            temp = sensor.temperature()
-            humidity = sensor.humidity()
-        except Exception as e:
-            print("DHT11 не доступен, используем встроенный датчик:", e)
-            temp = read_internal_temp()
-            humidity = 0
+        sensor.measure()
+        temp = sensor.temperature()
+        humidity = sensor.humidity()
+        print("Температура:", temp, "Влажность:", humidity)
 
-        print("Температура:", temp, "°C", "Влажность:", humidity)
         send_data(temp, humidity)
 
     except Exception as e:
-        print("Ошибка основного цикла:", e)
+        print("Ошибка с датчиком:", e)
+
     time.sleep(5)
